bbb47282dffc25ddc17295a2d614ab530855221a
Kenneth <keninchrist4eva@gmail.com>
2018-05-20 13:18:27 +0000

    nav almost done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@ $('.js-main-nav-overlay').on('click',function(e){
   if(target.is('.main-nav-cont') || target.parents().is('.main-nav-cont')){
     return;
   }else{
-    $('.main-nav-overlay').hide();
+    hideNav();
   }
 })
+
+$('.js-menu-button').on('click',showNav);
+
+$('.js-nav-close').on('click',hideNav)
+
+function showNav(){
+
+
+  $('.js-main-nav-cont').css('transform','translateX(0)');
+    $('.js-main-nav-overlay').css('width','100%');
+}
+
+function hideNav(){
+
+  $('.js-main-nav-cont').css('transform','translateX(280px)');
+    $('.js-main-nav-overlay').css('width','auto');
+}
+
+$('.menu-item.has-children').after().on('click',function(e){
+  $(e.target).children('.sub-menu').slideToggle();
+});
